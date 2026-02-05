09a8d35bc7f3cbb0d05f33f9cfa185850a5003fd
tfuxu <73042332+tfuxu@users.noreply.github.com>
2026-02-05 18:00:07 +0100

    feat: store latest error text in the ErrorPage instance - add a public method for setting a new error text

--- halftone/views/error_page.py
+++ halftone/views/error_page.py
@@ -1,6 +1,7 @@
 # Copyright 2025, tfuxu <https://github.com/tfuxu>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from typing import Optional
 from gi.repository import Adw, Gio, Gtk
 
 from halftone.constants import rootdir # pyright: ignore
@@ -10,11 +11,15 @@ from halftone.constants import rootdir # pyright: ignore
 class HalftoneErrorPage(Adw.Bin):
     __gtype_name__ = "HalftoneErrorPage"
 
+    copy_logs_button: Gtk.Button = Gtk.Template.Child()
+
     def __init__(self, parent: Gtk.Widget, **kwargs) -> None:
         super().__init__(**kwargs)
 
         self.parent = parent
         self.settings: Gio.Settings = parent.settings
+
+        self._latest_error: Optional[str] = None
 
         self.app: Adw.Application = self.parent.get_application()
         self.win: Adw.ApplicationWindow = self.app.get_active_window()
@@ -39,8 +44,20 @@ class HalftoneErrorPage(Adw.Bin):
     @Gtk.Template.Callback()
     def on_copy_logs_clicked(self, *args) -> None:
         clipboard = self.get_clipboard()
-        clipboard.set(self.parent.latest_traceback)
+        clipboard.set(self._latest_error)
 
         self.parent.toast_overlay.add_toast(
             Adw.Toast(title=_("Copied logs to clipboard"))
         )
+
+    """
+    Public Methods
+    """
+
+    def set_latest_error(self, error_text: Optional[str]) -> None:
+        if error_text:
+            self.copy_logs_button.set_sensitive(True)
+        else:
+            self.copy_logs_button.set_sensitive(False)
+
+        self._latest_error = error_text
